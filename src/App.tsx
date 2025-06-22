e6c0d509b0b5e047cde4ba7cb3256096b0accaaf
dexterwilliams96 <dexterwilliams4355@gmail.com>
2025-06-22 14:28:42 -0500

    dummy example, datagrid up and prints json to console

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import Box from "@mui/material/Box";
+import Skeleton from "@mui/material/Skeleton";
+import corp from "./data/corpora.json"
+import pap from "./data/papers.json"
+
+const rs = [
+  { id: 1, name: "Data Grid", description: "the Community version" },
+  { id: 2, name: "Data Grid Pro", description: "the Pro version" },
+  { id: 3, name: "Data Grid Premium", description: "the Premium version" },
+];
+
+const cols = [
+  { field: "name", headerName: "Product Name", width: 200 },
+  { field: "description", headerName: "Description", width: 300 },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading] = useState(false);
+  const [rows] = useState(rs);
+  const [columns] = useState(cols);
+  console.log(corp)
+  console.log(pap)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Box style={{ height: 300, width: "100%" }}>
+      {loading ? <Skeleton /> : <DataGrid rows={rows} columns={columns} />}
+    </Box>
+  );
 }
 
-export default App
+export default App;
