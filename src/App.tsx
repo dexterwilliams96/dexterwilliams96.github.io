0d4d55abd255b3bf407458a7484497dc5bd09a7a
dexterwilliams96 <dexterwilliams4355@gmail.com>
2025-06-22 19:25:33 -0500

    added dummy code

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,86 @@
 import { useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 import Skeleton from "@mui/material/Skeleton";
-import corp from "./data/corpora.json"
-import pap from "./data/papers.json"
+import Typography from "@mui/material/Typography";
+
+import corp from "./data/corpora.json";
+import pap from "./data/papers.json";
 
 const rs = [
-  { id: 1, name: "Data Grid", description: "the Community version" },
-  { id: 2, name: "Data Grid Pro", description: "the Pro version" },
-  { id: 3, name: "Data Grid Premium", description: "the Premium version" },
+  {
+    id: 1,
+    corpora_name: "lots of texts",
+    paper_name: "argpaper1",
+    authors: "tom, dick, harry",
+    date: "11/12/1999",
+    genre: "legal",
+    language: "English (en)",
+    document_types: "supreme court briefs",
+    document_count: 100,
+    annotation_description: "toulmin",
+    annotator_count: 3,
+    annotator_type: "expert",
+    agreement: 0.5,
+    agreement_interpretation: "moderate",
+    accessibility: "free",
+    corpora_link: "dummylink",
+    paper_link: "dummy link",
+  },
 ];
 
-const cols = [
-  { field: "name", headerName: "Product Name", width: 200 },
-  { field: "description", headerName: "Description", width: 300 },
+const columns = [
+  { field: "corpora_name", headerName: "Corpora Name", width: 200 },
+  { field: "paper_name", headerName: "Paper Name", width: 200 },
+  { field: "authors", headerName: "Authors", width: 200 },
+  { field: "date", headerName: "Date", width: 200 },
+  { field: "genre", headerName: "Genre", width: 200 },
+  { field: "language", headerName: "Language", width: 200 },
+  { field: "document_types", headerName: "Document Types", width: 200 },
+  { field: "document_count", headerName: "Document Count", width: 200 },
+  {
+    field: "annotation_description",
+    headerName: "Annotation Description",
+    width: 200,
+  },
+  { field: "annotator_count", headerName: "Annotator Count", width: 200 },
+  { field: "annotator_type", headerName: "Annotator Type", width: 200 },
+  { field: "agreement", headerName: "Agreement", width: 200 },
+  {
+    field: "agreement_interpretation",
+    headerName: "Interpretation",
+    width: 200,
+  },
+  { field: "accessibility", headerName: "Accessibility", width: 200 },
+  { field: "corpora_link", headerName: "Corpora Link", width: 200 },
+  { field: "paper_link", headerName: "Paper Link", width: 200 },
 ];
 
 function App() {
   const [loading] = useState(false);
   const [rows] = useState(rs);
-  const [columns] = useState(cols);
-  console.log(corp)
-  console.log(pap)
-
+  console.log(corp);
+  console.log(pap);
 
   return (
     <Box style={{ height: 300, width: "100%" }}>
-      {loading ? <Skeleton /> : <DataGrid rows={rows} columns={columns} />}
+      {loading ? (
+        <Skeleton />
+      ) : (
+        <Card variant="outlined">
+          <CardContent>
+            <Typography
+              gutterBottom
+              sx={{ color: "text.secondary", fontSize: 14 }}
+            >
+              Grouped by Corpora
+            </Typography>
+            <DataGrid rows={rows} columns={columns} />
+          </CardContent>
+        </Card>
+      )}
     </Box>
   );
 }
